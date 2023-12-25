b3ef0545771d858550519a8de39e7b9661eec9ec
subramanayam12 <subramanyam933@gmail.com>
2023-12-25 21:45:44 +0530

    adding changes

--- padproject/asgi.py
+++ padproject/asgi.py
@@ -29,7 +29,6 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'padproject.settings')
 # is populated before importing code that may import ORM models.
 django_asgi_app = get_asgi_application()
 
-import padapp.routing
 
 application = ProtocolTypeRouter(
     {
